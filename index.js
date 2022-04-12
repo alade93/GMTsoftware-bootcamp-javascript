535a218d51799b35e0c0da1802b6c11e8b3eae5a
alade93 <99254792+alade93@users.noreply.github.com>
2022-04-12 01:47:59 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,54 @@
+// let string = "30 Days of javascript";
+// console.log(string);
+// console.log(string.repeat(2));
+// not necesary.....
+//Start of second class work level 2
+
+// statements....
+let text =
+  "There is no exercise better for the heart than reaching down and lifting people up by \
+  John Holmes teaches us to help one another";
+console.log(text);
+let text1 =
+  "Love is not patronizing and charity isn't about pity, it is about love. Charity and love are the same \
+   with charity you give love, so don't just give money but reach out your hand instead.";
+console.log(text1);
+
+//checking for "on"......
+let ja = "jargon";
+let pyt = "python";
+console.log(ja.includes("on"));
+console.log(pyt.includes("on"));
+
+// check for "jargon"......
+let phrase = "I hope this course is not full of jargon";
+console.log(phrase.includes("jargon"));
+
+//random number generator.....
+let randomNum = Math.random(100);
+console.log(Math.random);
+let numBtnZeroAndHundred = randomNum * 101;
+console.log(numBtnZeroAndHundred);
+let randomNum2 = Math.random(50);
+console.log(Math.random);
+let numBtnFiftyAndHundred = randomNum2 * 51;
+console.log(numBtnFiftyAndHundred);
+let randomNum3 = Math.random(255);
+console.log(Math.random);
+let numBtnZeroAndTwoHundredFifty = randomNum3 * 256;
+console.log(numBtnZeroAndTwoHundredFifty);
+
+//Accessing "javascript" using random number...
+let jav = "JavaScript";
+console.log(jav.charAt(5));
+
+//parsefloat to equal to 10.....
+let numFloat = parseFloat("9.8");
+console.log(numFloat);
+const num = 9.8;
+console.log(Math.round("9.8"));
+
+//check typeof "10"
+let no = "10";
+let numInt = parseInt(10);
+console.log(numInt);
